0cd6f57c72542c6aa1cc7f6c25d76bcf94a88545
Mariella <mariellabogoni@id.uff.br>
2017-10-03 15:07:10 -0300

    Iniciando o calculo dos coeficientes

--- ajuste.c
+++ ajuste.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <math.h>
 FILE* leitura;
+
+void coeficientes(double x1[10], double y1[10])
+{	 double a0, a1;
+    int i;
+    float soma1=0, soma2=0;
+    
+    for(i=0;i<10;i++)
+    {   
+       soma1 = soma1 + x[i]*x[i];
+       
+    
+     
+    }
+
+}
 int main()
 {  
    int i;
-   double x[10], y1[10];
+   double x[10], y1[10], a0, a1;
    
    
    leitura=fopen("dados.dat","r");
@@ -25,6 +41,8 @@ int main()
    
    
    fclose(leitura);
+   
+   
    
    
 }   
